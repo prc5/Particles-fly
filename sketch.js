2bb56da3366661f5e9e4f84f862bf918dae287ff
prc5 <m.pyrc@interia.pl>
2017-09-16 21:19:01 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+var fr;
+var zoff = 0;
+
+var particles = [];
+
+var flowfield;
+
+
+function setup() {
+    createCanvas(300, 300);
+    cols = floor(width / scl);
+    rows = floor(height / scl);
+    fr = createP('');
+    
+    flowfield = new Array(cols*rows);
+    
+    for (var i = 0; i < 300; i++){
+        particles[i] = new Particle();
+    }
+}
+
+function draw() {
+    background(255);
+
+    var yoff = 0;
+    for (var y = 0; y < rows; y++) {
+        var xoff = 0;
+        for (var x = 0; x < cols; x++) {
+            var index = x + y * cols;
+            var angle = noise(xoff,yoff,zoff) * TWO_PI * 4;
+            var v = p5.Vector.fromAngle(angle);
+            //v.setMag(10);
+            
+            flowfield[index] = v;
+            xoff += inc;
+            
+            stroke(0, 50);
+            //strokeWeight(1);
+            //push();
+            //translate(x*scl, y*scl);
+            //rotate(v.heading());
+            //line(0,0,scl,0);        
+            //pop();
+        }
+        yoff += inc;
+        zoff += 0.0003;
+    }
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].follow(flowfield);
+        particles[i].update();
+        particles[i].show();
+        particles[i].edges();
+    }
+    fr.html(floor(frameRate()));
+}
